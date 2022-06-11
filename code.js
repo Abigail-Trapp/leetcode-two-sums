35883f503bc9c942e67a197eeda5c3314ec83711
Abigail Trapp <abbsthepandahead@gmail.com>
2022-06-10 22:30:26 -0600

    second written test

--- code.js
+++ code.js
@@ -13,5 +13,9 @@ console.log(twoSum([2,7,11,15], 9))
 
 let body = document.querySelector("body")
 let p = document.createElement("p")
-p.innerHTML = `[2,7,11,15] => ${twoSum([2,7,11,15], 9)}`
+p.innerHTML = `[2,7,11,15], target = 9 => ${twoSum([2,7,11,15], 9)}`
 body.append(p)
+
+let p2 = document.createElement("p")
+p2.innerHTML = `[3,2,4,6], target = 6 => ${twoSum([3,2,4,6], 6)}`
+p.append(p2)
