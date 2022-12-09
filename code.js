68abfd6277d6a7d385e86ad14bc7327fad9d9f57
Abigail Trapp <abbsthepandahead@gmail.com>
2022-12-08 19:25:26 -0700

    reduced lines of code

--- code.js
+++ code.js
@@ -1,12 +1,11 @@
 var twoSum = function(nums, target) {
  let map = new Map ()
 for(let i=0;i<nums.length;i++){
-  let firstNum = nums[i]
-  let secondNum = target - firstNum
+  let secondNum = target - nums[i]
   if(map.has(secondNum)){
     return [i, map.get(secondNum)]
   }
-    map.set(firstNum, i)
+    map.set(nums[i], i)
 }
 } 
 console.log(twoSum([2,7,11,15], 9))
